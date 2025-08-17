d16dc33cd2625070e61078fb886c77bf45061e73
gikdev <m.m.bhrm85@gmail.com>
2025-08-17 12:09:28 +0330

    Fixed the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,27 @@
-cd apps/website
+mkdir ./dist
+mkdir ./dist/appverse-core ./dist/appverse-desktop ./dist/website
+
+# Build website
+cd ./apps/website
 npm run build
+cp -r ./dist ../../dist/website
 
 cd ../..
-cp -r ./apps/website/dist ./dist
 
-cd apps/appverse
-npm run build
+# Build appverse core for website
+cd ./apps/appverse/core
+npm run build:website
+cp -r ./dist ../../../dist/website/appverse
 
-cd ../..
-cp -r ./apps/appverse/dist ./dist/appverse
+cd ../../..
 
-explorer .
+# Build appverse core for desktop
+cd ./apps/appverse/core
+npm run build:desktop
+cp -r ./dist ../desktop/dist
+
+cd ..
+
+# Build the desktop
+cd ./desktop
+npm run make
