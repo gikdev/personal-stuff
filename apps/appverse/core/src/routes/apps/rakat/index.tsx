b2372259deb7bb99d89137c093bbcc15a2eb8718
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2025-08-26 21:18:23 +0330

    Biome format

--- apps/appverse/core/src/routes/apps/rakat/index.tsx
+++ apps/appverse/core/src/routes/apps/rakat/index.tsx
@@ -8,7 +8,9 @@ export const Route = createFileRoute("/apps/rakat/")({
 function RouteComponent() {
   const rakatCount = useRakatStore(s => s.rakatCount)
 
-  const isValidRakat = [2,3,4].includes(rakatCount)
+  const isValidRakat = [2, 3, 4].includes(rakatCount)
 
-  return <Navigate to={isValidRakat ? "/apps/rakat/pray" : "/apps/rakat/choose"} />
+  return (
+    <Navigate to={isValidRakat ? "/apps/rakat/pray" : "/apps/rakat/choose"} />
+  )
 }
